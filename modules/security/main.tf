07f1962853ff2529cd87b290e9efddefa52cd37f
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-05-19 00:20:41 -0300

    Update main.tf

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,3 +1,18 @@
 resource "aws_security_group" "secure_sg" {
-  name = "enterprise-secure-group"
+  name        = "enterprise-secure-group"
+  description = "Security baseline group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/24"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
